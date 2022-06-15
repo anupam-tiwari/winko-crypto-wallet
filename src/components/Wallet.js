5e4e7f4d10bee511a9c1a46e8bd708232ced342d
anupam-tiwari <appleanupam@gmail.com>
2022-06-16 01:39:58 +0800

    first commit

---
+++ src/components/Wallet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ethers} from 'ethers'
+import interactions from '../interactions/interactions'
+
+
+export default function Wallet() {
+  return (
+    <div>Wallet</div>
+  )
+}
---
+++ src/interactions/interactions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function interactions() {
+  return (
+    <div>interactions</div>
+  )
+}
